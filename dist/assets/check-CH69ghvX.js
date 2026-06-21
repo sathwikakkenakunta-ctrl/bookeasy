43f4b22e8f612fea36e618cf42e830a100fc6eb2
Sathwik <sathwikakkenakunta@gmail.com>
2026-06-21 15:57:43 +0530

    Fix Vercel build script

--- dist/assets/check-CH69ghvX.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./UI-DPyAaDLb.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
--- dist/assets/chevron-right-BlUsxwX4.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./UI-DPyAaDLb.js";var t=e(`ChevronRight`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
